823c96efa521b66cc9674e3d0a9e6ec13366ed05
kaskil12 <89510293+kaskil12@users.noreply.github.com>
2023-01-25 12:42:23 +0100

    la til og si hei til folk og si navnet dems
    mø

--- BrageBot.js
+++ BrageBot.js
@@ -6,5 +6,8 @@ let wordCount = {};
 bot.on("ready", () => {
     console.log(Logged in as ${bot.user.username});
 });
-
+client.on('message',
+function (messages) {         
+if(messages.content.toLocaleLowerCase()==='hello'
+   messages.channel.send('hello' + ''- + messages.author.username);
 bot.login("bot token her");
